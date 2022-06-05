eed7b97c1f0a480bdd1ebb8b8611dd34f39b3176
Tboske <jeffke.de.meutter@gmail.com>
2022-06-05 14:43:05 +0200

    textoffset fixed

--- OverlordProject/Prefabs/Text.cpp
+++ OverlordProject/Prefabs/Text.cpp
@@ -1,20 +1,66 @@
 #include "stdafx.h"
 #include "Text.h"
 
+#include "CubePrefab.h"
 
-
-Text::Text()
-	: m_pFont(ContentManager::Load<SpriteFont>(L"SpriteFonts/Consolas_32.fnt"))
+Text::Text(XMFLOAT2 pos, XMFLOAT2 padding, TextPivot pivot, const std::wstring& text, const XMFLOAT4& color)
+	: m_Position(pos)
+	, m_PivotOffset(pivot)
+	, m_Padding(padding)
+	, m_Color(color)
+	, m_Text(text)
+	, m_pFont(ContentManager::Load<SpriteFont>(L"SpriteFonts/Arial_32.fnt"))
 {
-	
-}
-
-void Text::Initialize(const SceneContext& )
-{
-
+	UpdateOffset();
 }
 
 void Text::Update(const SceneContext&)
 {
+	if (!m_Visible)
+		return;
+
+	if (m_pFont->GetName() == L"")
+		return;
+
 	TextRenderer::Get()->DrawText(m_pFont, m_Text, m_Position, m_Color);
+}
+
+
+void Text::UpdateOffset()
+{
+	const auto& size = m_pFont->GetSize();
+
+	switch (m_PivotOffset)
+	{
+	case leftTop:
+		m_Position = m_Padding;
+		break;
+
+	case rightTop:
+		m_Position.x -= size / 2 * m_Text.size();
+
+		m_Position.x -= m_Padding.x;
+		m_Position.y += m_Padding.y;
+		break;
+
+	case leftBottom:
+		m_Position.y -= size;
+
+		m_Position.x += m_Padding.x;
+		m_Position.y -= m_Padding.y;
+		break;
+
+	case rightBottom:
+		m_Position.x -= size / 2 * m_Text.size();
+		m_Position.y -= size;
+
+		m_Position.x -= m_Padding.x;
+		m_Position.y -= m_Padding.y;
+		break;
+
+	case center:
+		m_Position.x -= (size / 2 * m_Text.size()) /2;
+		m_Position.y -= size / 2;
+		break;
+	}
 }
--- OverlordProject/Prefabs/Text.h
+++ OverlordProject/Prefabs/Text.h
@@ -1,29 +1,45 @@
 #pragma once
 
+enum TextPivot
+{
+	leftTop,
+	rightTop,
+	leftBottom,
+	rightBottom,
+	center
+};
+
 class Text final : public GameObject
 {
 public:
-	Text();
+	Text(XMFLOAT2 pos, XMFLOAT2 padding, TextPivot pivot = leftTop, const std::wstring& text = L"Text"
+		, const XMFLOAT4& color = { 1,1,1,1 }
+	);
 	~Text() override = default;
 	Text(const Text&) = delete;
 	Text(Text&&) noexcept = delete;
 	Text& operator=(const Text&) = delete;
 	Text& operator=(Text&&) noexcept = delete;
 
-	void SetColor(const XMFLOAT3& color) { m_Color = {color.x, color.y, color.z, 1.f}; }
-	void SetPosition(const XMFLOAT2& pos) { m_Position = pos; }
+	
 	void SetText(const std::wstring& text) { m_Text = text; }
+	void SetVisible(bool visible) { m_Visible = visible; }
 
 protected:
-	virtual void Initialize(const SceneContext&) override;
 	virtual void Update(const SceneContext&) override;
+	
 
 private:
-	SpriteFont* m_pFont;
-
 	XMFLOAT2 m_Position{0,0};
-	XMFLOAT4 m_Color {1,0,0,1};
+	TextPivot m_PivotOffset = leftTop;
+	XMFLOAT2 m_Padding{ 5,5 };
 
+	bool m_Visible = true;
+	SpriteFont* m_pFont;
+	XMFLOAT4 m_Color {1,0,0,1};
 	std::wstring m_Text{L"0"};
+
+
+	void UpdateOffset();
 };
 
